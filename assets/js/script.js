a2776516531407363c936184a8c9870644d23cdd
DavidTausend <davidtausend-911@hotmail.com>
2024-01-04 09:12:35 +0000

    add window for music settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,44 @@
+/* JS Code quality tool: https://jshint.com/ */
+/* https://www.w3schools.com/w3css/w3css_modal.asp */
 function showInstructions() {
-    let modal = document.getElementById("instructions");
+    let window = document.getElementById("instructions");
+    let span = document.getElementsByClassName("close-window")[0];
 
-     // Get the <span> element that closes the modal
-     var span = document.getElementsByClassName("close-window")[0];
+    // Open the window (model)
+    window.style.display = "block";
 
-     // Open the modal 
-     modal.style.display = "block";
- 
-     // When the user clicks on <span> (x), close the modal
-     span.onclick = function() {
-         modal.style.display = "none";
-     }
- 
-     // When the user clicks anywhere outside of the modal, close it
-     window.onclick = function(event) {
-         if (event.target == modal) {
-             modal.style.display = "none";
-         }
-     }
- }
+    // Close window (model) when the user clicks on (x)
+    span.onclick = function() {
+        window.style.display = "none";
+    }
 
-function musicSettings() {
+    // Close the window (model) when the user clicks anywhere outside of the window
+    window.onclick = function(event) {
+        if (event.target == window) {
+            window.style.display = "none";
+        }
+    }
+}
 
+ function musicSettings() {
+    let window = document.getElementById("music");
+    let span = document.getElementsByClassName("close-window")[0];
 
+    window.style.display = "block";
+
+    span.onclick = function() {
+        window.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == window) {
+            window.style.display = "none";
+        }
+    };
 }
 
 // Play music game
-let audio = new Audio("assets/audio/pokemom-instrumental.mp3");
+let audio = new Audio("../audio/pokemom-instrumental.mp3");
 audio.play();
 
 // End of music
@@ -43,7 +55,7 @@ function shuffleArray(array) {
 // Cards generator: https://dev.to/javascriptacademy/creating-a-memory-card-game-with-html-css-and-javascript-57g1
 // Shuffle cards
 function shuffleCards() {
-    const cardContainer = document.getElementsByClassName('.cards');
+    const cardContainer = document.getElementsByClassName('cards')[0];
     let cards = Array.from(cardContainer.children);
     cards = shuffleArray(cards);
     cards.forEach(card => cardContainer.appendChild(card));
