bb913608856d4bcf745a8c5f2b5eddab9b3d2c6c
DavidTausend <davidtausend-911@hotmail.com>
2024-01-04 08:17:13 +0000

    add window for intructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 function showInstructions() {
+    let modal = document.getElementById("instructions");
 
-}
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close-window")[0];
+
+     // Open the modal 
+     modal.style.display = "block";
+ 
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+     }
+ 
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     }
+ }
 
 function musicSettings() {
 
